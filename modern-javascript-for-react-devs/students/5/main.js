d1e3d0a7aa4ee2b4108655651714d32421114571
Farjana Akter <fakter@xpanxion.com>
2023-05-10 11:02:07 -0400

    update my ex2

--- modern-javascript-for-react-devs/students/5/main.js
+++ modern-javascript-for-react-devs/students/5/main.js
@@ -3,14 +3,16 @@
 // Date: 5/24/2022
 // 
 
-const ex1 = () => {
+/*const ex1 = () => {
     //const array = [1, '2', 3, 'test', 1.2];
     const array =[3,4,5,8]
     console.log(countNumbers(array));
-}
+}*/
 
-const ex2 = () => {
-    // TODO...
+const ex2 = () => { 
+    const array = [12, 55, 2, 22, 11];
+    console.log(minNumber(array));
+
 }
 
 const ex3 = () => {
@@ -21,18 +23,38 @@ const ex3 = () => {
 // Your functions here...
 //
 
-const countNumbers = (array) => {
+/*const countNumbers = (array) => {
     let countNumbers = 0;
     for (let i =0; i < array.length; i++) {
         if (typeof(array[i]) == 'number') countNumbers++;
     }
     // TODO...
     return countNumbers;
+}*/
+
+///////////////
+
+const minNumber = (array) => {
+    let minNumber = array[0];
+for (let i = 1; i < array.length; i++) {
+  if (array[i] <  minNumber){
+    minNumber = array[i];
+   } }
+   return minNumber;
 }
 
+
+//const main = async () => {
+ //   ex1();
+//}
 
 const main = async () => {
-    ex1();
+    ex2()
 }
 
+
+
+
 main();
+
+
