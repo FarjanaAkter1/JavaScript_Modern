ef735ed24400c474306cc66b7f4e9a8c903190a3
Farjana Akter <fakter@xpanxion.com>
2023-05-10 21:57:47 -0400

    update my ex4

--- modern-javascript-for-react-devs/students/5/main.js
+++ modern-javascript-for-react-devs/students/5/main.js
@@ -15,16 +15,28 @@
 
 }*/
 
-const ex3 = () => {
+/*const ex3 = () => {
 
    // const array1 = [1, 2, 3, 4, 5];
     
      const array1 = [1, 2,];
     const array2 = ['a', 'b', 'c', 'd', 'e'];
     console.log(interleave(array1, array2));
+}*/
+
+
+const ex4 = () => {
+    console.log(palidrome('radar'));
+   console.log(palidrome('month'));
+
 }
 
-//
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Your functions here...
 //
 
@@ -47,7 +59,7 @@ for (let i = 1; i < array.length; i++) {
    return minNumber;
 }*/
 
-////////////////////
+////////////////////////////////////////////////////
 
 /*const interleave =(array1 ,array2) => {
 
@@ -60,38 +72,51 @@ for (let i = 1; i < array.length; i++) {
    return newArr; 
 }*/
 
-const interleave =(array1 ,array2) => {
+/*const interleave =(array1 ,array2) => {
 if (array1.length !== array2.length){
     return  `ERROR: Array length mismatch`
 
 }
 
+}*/
+///////////////////////////////////////////////////////////////////////
 
-
+const palidrome =(word) =>{
+let newword =""
+for (let i =word.length -1; i>=0; i--){
+    newword += word[i];
+}
+if (newword == word){
+    return true;
+}else{
+    return false;
+}
 
 }
 
+  
 
 
 
 
 
 
-
-
-//const main = async () => {
- //   ex1();
-//}
 
 /*const main = async () => {
+    ex1();
+}
+
+const main = async () => {
     ex2()
-}*/
+}*
 
 const main = async () => {
     ex3()
+}*/
+
+const main = async () => {
+    ex4()
 }
-
-
 main();
 
 
